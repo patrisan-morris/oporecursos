6dd46e0bc2cd76f72df0a945ebca73d16508433b
Patrisan Morris <patrisanmorris@gmail.com>
2025-05-02 20:30:34 +0200

    chore: Initialize base project Opo Recursos

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: 'resources/js/app.js',
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+    ],
+    server: {
+        open: true,
+        host: '127.0.0.1',
+        port: 5173,
+    },
+});
