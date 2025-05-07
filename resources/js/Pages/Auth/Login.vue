5179c903fd4fd63ad7bcfe3d80e5d278e15f33a2
Patrisan Morris <patrisanmorris@gmail.com>
2025-05-07 17:14:22 +0200

    chore:Login page layout

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,54 +1,59 @@
 <template>
     <GuestLayout>
         <Head title="Log in" />
-        <section class="bg-gradient-to-b from-darkPrimary to-lightPrimary min-h-screen w-full flex flex-col justify-center items-center text-center relative px-6">
-            <div class="max-w-4xl w-full flex flex-row justify-center items-center text-transparent bg-clip-text bg-gradient-to-r from-white to-lightbg ">
-                <div class="w-full md:w-1/2 flex flex-col items-start justify-start">
-                    <h1 class="text-left text-3xl md:text-4xl text-white text-shadow-sm leading-md md:leading-[5rem]">
-                        Sign in to your account
-                    </h1>
-                    <p class="text-lg md:text-lg text-white">
-                        Enter your credentials to access your account.
-                    </p>
-                </div>
-                <div class="w-full md:w-1/2 bg-white rounded-xl shadow-lg p-10">
-                    <form @submit.prevent="submit">
-                        <div class="flex flex-col items-start gap-2">
-                            <InputLabel class="text-primary text-[20px] tracking-wide" for="email" value="Email:" />
-                            <TextInput id="email" type="email" class="mt-1 block w-full" v-model="form.email" required autofocus autocomplete="username"/>
-                            <InputError class="mt-2" :message="form.errors.email" />
+        <section class="bg-gradient-to-b from-darkPrimary to-lightPrimary min-h-screen flex items-center justify-center">
+            <transition enter-active-class="transition ease-out duration-500" enter-from-class="opacity-0 translate-y-5" enter-to-class="opacity-100 translate-y-0" >
+                <div v-show="showForm" class="max-w-xs lg:max-w-5xl w-full flex flex-col md:flex-row items-center md:items-center text-white">
+                    <div class="flex flex-col w-full md:w-1/2 pr-8">
+                        <h1 class="text-4xl font-bold mb-4 leading-tight">Sign in to your account</h1>
+                        <p class="text-lg text-gray-200">Enter your credentials to access your account.</p>
+                    </div>
+                    <!-- Form -->
+                    <div class="w-full md:w-1/2 bg-white text-primary rounded-2xl shadow-md p-8">
+                        <!-- General status -->
+                        <div v-if="status" class="mb-4 text-sm font-medium text-green-600">
+                            {{ status }}
                         </div>
 
-                        <div class="flex flex-col items-start gap-2 mt-4">
-                            <InputLabel class="text-primary text-[20px] tracking-wide" for="password" value="Password:" />
-                            <TextInput id="password" type="password" class="mt-1 block w-full" v-model="form.password" required autocomplete="current-password"/>
-                            <InputError class="mt-2" :message="form.errors.password" />
-                        </div>
+                        <form @submit.prevent="submit">
+                            <div class="space-y-4">
+                                <div>
+                                    <InputLabel for="email" value="Email:" class="text-primary text-[1.1rem] mb-2"/>
+                                    <TextInput id="email" type="email" class="mt-1 block w-full focus:border-lightPrimary focus:ring-lightPrimary" v-model="form.email" required autofocus autocomplete="username" />
+                                    <InputError class="mt-1" :message="form.errors.email" />
+                                </div>
 
-                        <div class="flex flex-row justify-between gap-2 mt-4">
-                            <div class="mt-4 block">
-                                <label class="flex items-center">
-                                    <Checkbox name="remember" v-model:checked="form.remember" class="pointer"/>
-                                    <span class="ms-2 text-md text-primary">Remember me</span>
+                                <div>
+                                    <InputLabel for="password" value="Password:" class="text-primary text-[1.1rem] mb-2"/>
+                                    <TextInput id="password" type="password" class="mt-1 block w-full focus:border-lightPrimary focus:ring-lightPrimary" v-model="form.password" required autocomplete="current-password" />
+                                    <InputError class="mt-1" :message="form.errors.password" />
+                                </div>
+
+                                <div class="flex flex-col sm:flex-row sm:items-center sm:justify-between mt-4">
+                                <label class="flex items-center space-x-2">
+                                    <Checkbox name="remember" v-model:checked="form.remember" class="accent-primary text-primary focus:ring-primary"/>
+                                    <span class="text-sm">Remember me</span>
                                 </label>
-                            </div>
-                            <div class="mt-4">
-                                <Link v-if="canResetPassword" :href="route('password.request')" class="text-md text-primary underline hover:text-lightPrimary focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 active:ring-0">
+
+                                <Link v-if="canResetPassword" :href="route('password.request')" class="mt-2 sm:mt-0 text-sm text-primary hover:underline">
                                     Forgot your password?
                                 </Link>
+                                </div>
+
+                                <div class="mt-6">
+                                <PrimaryButton class="w-full bg-primary hover:bg-white hover:border-primary hover:text-primary" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                                    Log in
+                                </PrimaryButton>
+                                </div>
                             </div>
-                        </div>
-                        <div class="mt-10 flex items-center justify-center">
-                            <PrimaryButton class="ms-4 bg-primary" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                                Log in
-                            </PrimaryButton>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
-            </div>
+            </transition>
         </section>
     </GuestLayout>
 </template>
+
 <script setup>
     import Checkbox from '@/Components/Checkbox.vue';
     import GuestLayout from '@/Layouts/GuestLayout.vue';
@@ -57,6 +62,7 @@
     import PrimaryButton from '@/Components/PrimaryButton.vue';
     import TextInput from '@/Components/TextInput.vue';
     import { Head, Link, useForm } from '@inertiajs/vue3';
+    import { ref, onMounted } from 'vue';
 
     defineProps({
         canResetPassword: {
@@ -78,4 +84,13 @@
             onFinish: () => form.reset('password'),
         });
     };
+
+    const showForm = ref(false);
+    onMounted(() => {
+        // Show the form after a short delay
+        setTimeout(() => {
+            showForm.value = true;
+        }, 300);
+    });
+
 </script>
